36fc8a8c00d7335d38683e846ecf743407ef6d68
luis solsona <mistik777@gmail.com>
2025-03-27 23:09:03 +0100

    Update buscar-duplicados-por-carpeta.sh

--- buscar-duplicados-por-carpeta.sh
+++ buscar-duplicados-por-carpeta.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Solicitar el año de la carpeta donde buscar duplicados. Se entiende que tienes las fotos separadas por años en carpetas.
-read -p "Introduce el año (por ej 2023): " year
+# Solicitar el año
+read -p "Introduce el año (por ejemplo, 2005): " year
 
-# Verificar que el año no esté vacío (año equivale a carpeta)
+# Verificar que el año no esté vacío
 if [[ -z "$year" ]]; then
     echo "Error: Debes introducir un año válido."
     exit 1
 fi
 
-# Buscar duplicados y guarda el resultado en un txt con el año
+# Buscar duplicados
 rdfind -outputname duplicados-$year.txt /mnt/server/Fotos/$year
 
 # Archivo de resultados de rdfind
@@ -29,25 +29,23 @@ mkdir -p "$DEST_DIR"
 total_found=0
 total_moved=0
 
-# Leer y mover archivos duplicados del txt que ha generado rdfind
-grep "DUPTYPE_WITHIN_SAME_TREE" "$DUPLICATES_FILE" | while IFS= read -r line; do
+# Leer y mover archivos duplicados
+while IFS= read -r line; do
     # Extraer la ruta completa del archivo manteniendo los espacios y eliminando números antes de la ruta
     file=$(echo "$line" | sed -e 's/^.*[0-9]\{1,\} //')
 
     # Verificar si el archivo existe
     if [[ -f "$file" ]]; then
-        total_found = $((total_found + 1))
+        ((total_found++))
         echo "Moviendo: \"$file\" -> \"$DEST_DIR\""
         mv "$file" "$DEST_DIR"
-        total_moved = $((total_moved + 1))
+        ((total_moved++))
     else
         echo "Advertencia: Archivo no encontrado: \"$file\""
     fi
-done
+done < <(grep "DUPTYPE_WITHIN_SAME_TREE" "$DUPLICATES_FILE")
 
 # Resumen
-
 echo "$total_found ficheros duplicados"
 echo "$total_moved ficheros movidos"
-
 echo "Proceso completado."
